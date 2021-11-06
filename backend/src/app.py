bd7dab8b1525064b33b6d016c47814f27fe8a66d
Radu Schirliu <radu.schirliu@gmail.com>
2021-11-06 10:30:06 -0600

    fix: backend errors

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from pycoingecko import CoinGeckoAPI
 
-from .auth import verify_jwt
+from auth import verify_jwt
 
 # Load dotenv files
 load_dotenv()
@@ -19,82 +19,82 @@ cors = CORS(app)
 cg = CoinGeckoAPI()
 
 @app.route("/")
-@cross_origin
+@cross_origin()
 def index():
     return "hello world"
 
 # COINLIST
 
 @app.route('/coinlist', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_coinlist():
     return cg.get_coins_list()
 
 # WATCHLIST
 
 @app.route('/watchlist', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_watchlist():
     return ""
 
 @app.route('/watchlist/<watching_id>', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_watching():
     return ""
 
 @app.route('/watchlist', methods=['POST'])
-@cross_origin
+@cross_origin()
 def post_watching():
     return ""
 
 @app.route('/watchlist/<watching_id>', methods=['DELETE'])
-@cross_origin
+@cross_origin()
 def delete_watching():
     return ""
 
 # PURCHASES
 
 @app.route('/purchases', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_purchases():
     return ""
 
 @app.route('/purchases/<purchase_id>', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_purchase():
     return ""
 
 @app.route('/purchase', methods=['POST'])
-@cross_origin
+@cross_origin()
 def post_purchase():
     return ""
 
 # ORDERS
 
 @app.route('/orders', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_orders():
     return ""
 
 @app.route('/orders/<order_id>', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_order():
     return ""
 
 @app.route('/orders', methods=['POST'])
-@cross_origin
+@cross_origin()
 def post_order():
     return ""
 
 @app.route('/orders/<order_id>', methods=['DELETE'])
-@cross_origin
+@cross_origin()
 def delete_order():
     return ""
 
 # PORTFOLIOS
 
 @app.route('/portfoilios', methods=['GET'])
-@cross_origin
+@cross_origin()
 def get_portfolios():
     return ""
 
