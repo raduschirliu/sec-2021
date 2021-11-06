5e626dc70da27a92e6315372093c1d92a3a229b1
Radu Schirliu <radu.schirliu@gmail.com>
2021-11-06 09:14:06 -0600

    feat: backend auth

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,13 @@
 import os
+import datetime
 from dotenv import load_dotenv
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
+from .auth import verify_jwt
+
 # Load dotenv files
 load_dotenv()
-
 
 # Initialize Flask
 port = int(os.getenv('PORT', 8000))
@@ -13,11 +15,25 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
-# Hello world
 @app.route("/")
 @cross_origin
-def hello_world():
+def index():
     return "hello world"
+
+# Update user account information
+@app.route('/account', methods=['POST'])
+@cross_origin()
+def update_account():
+    jwt = verify_jwt()
+
+    if jwt == False:
+        return "Unauthorized", 401
+
+    print(jwt)
+    data = request.json
+    print(data)
+
+    return "this does nothing!!"
 
 # Run Flask app
 if __name__ == '__main__':
